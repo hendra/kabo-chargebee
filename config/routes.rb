6c8ad178c69a76528d1c35e335228c627f6edad9
Hendra Nicholas <hendra.nicholas@telesoftmobile.com>
2021-04-04 02:46:01 +0700

    add root to route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
   get 'subscriptions/new'
-  post 'subscriptions/update'
+  put 'subscriptions/update'
+  root to: 'subscriptions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/routing/subscriptions_routing_spec.rb
+++ spec/routing/subscriptions_routing_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe SubscriptionsController, type: :routing do
       expect(get: "/subscriptions/new").to route_to("subscriptions#new")
     end
 
-    it "routes to #update via POST" do
-      expect(post: "/subscriptions/update").to route_to("subscriptions#update")
+    it "routes to #update via PUT" do
+      expect(put: "/subscriptions/update").to route_to("subscriptions#update")
     end
   end
 end
